68271588215286d6585c3129888c2923a4ad7384
Dave Ackerman <dave@moduscreate.com>
2016-03-04 14:51:27 -0700

    transaction view stuff

--- app/pages/transaction/transaction.view.ts
+++ app/pages/transaction/transaction.view.ts
@@ -1,6 +1,8 @@
+import { groupBy, sortBy } from 'lodash';
 import { Page, NavController } from 'ionic-angular';
 import { MotionLogo } from 'components/motionLogo/motionLogo.component';
 import * as template from './transaction.view.html';
+import * as customerData from 'data/customers.json';
 
 @Page({
      template: template,
@@ -8,7 +10,26 @@ import * as template from './transaction.view.html';
 })
 
 export class TransactionView {
-  constructor(public nav: NavController) {}
+  constructor(nav: NavController) {
+
+      this.shouldHideCancel = false;
+
+      this.nav = nav;
+
+      // group customers by their starting letter
+      this.customers = groupBy(customerData.customers, (customer) => customer.name[0]);
+
+      // get the keys, sort
+      this.customersKeys = sortBy(Object.keys(this.customers))
+  }
+
+  onInput(e) {
+      console.log(e);
+  }
+
+  onCancel(e) {
+      console.log(e);
+  }
 
   close() {
       this.nav.pop();
